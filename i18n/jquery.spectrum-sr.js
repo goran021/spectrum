39fc56e744fcdc08fe4ec0d6b5a62c215d0c4f16
goran021 <brankovic.bg@gmail.com>
2017-03-13 17:53:45 +0100

    Create jquery.spectrum-sr.js

---
+++ i18n/jquery.spectrum-sr.js
@@ -0,0 +1,18 @@
+// Spectrum Colorpicker
+// Serbian Cyrillic (sr) localization
+// https://github.com/bgrins/spectrum
+
+(function ( $ ) {
+
+    var localization = $.spectrum.localization["sr"] = {
+        cancelText: "Откажи",
+        chooseText: "Одабери",
+        clearText: "Поништи одабир",
+        noColorSelectedText: "Боја није одабрана",
+        togglePaletteMoreText: "Више",
+        togglePaletteLessText: "Мање"
+    };
+
+    $.extend($.fn.spectrum.defaults, localization);
+
+})( jQuery );
